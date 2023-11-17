b27ed0f2fd2846634e2efb36a8ba2942f9e50e90
Rafa Peregrino <rbperegrino@gmail.com>
2023-11-17 16:55:05 -0300

    theming

--- src/app/view/board/column/column.component.ts
+++ src/app/view/board/column/column.component.ts
@@ -12,8 +12,12 @@ import {Component, computed, signal, ViewEncapsulation} from '@angular/core';
 export class ColumnComponent {
   checked = signal(true)
   classes = computed( () =>[
-   this.checked() ? 'bg-gradient-to-b from-[#F7F1FF] from-60% to-white h-full'  : 'bg-[#F7F1FF] h-[85px]',
+   this.checked() ? 'bg-gradient-to-b from-lead-100 from-60% to-white h-full'  : 'bg-lead-100 h-[85px]',
   ])
+  titleClasses = ['text-lead-400']
+  boxClasses = ['border-lead-200']
+  title = 'Lead'
+
 
   check = () => this.checked.update(value => {
     return !value;
